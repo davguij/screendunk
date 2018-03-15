15905aaff40e64e1090949d66c0bc2e797153099
David Guijarro <guijarro.dav@gmail.com>
2018-03-15 17:58:01 +0100

    feat: default screen size

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 const fastify = require('fastify')();
-const screenshot = require('./screenshots');
+const screenshot = require('./screenshoter');
 
 fastify.get('/status', (request, reply) => {
   reply.send({ status: 'All good!' });
 });
 
 fastify.get('/screenshot', async (request, reply) => {
-  console.log(request.query.url);
+  const UA = request.req.headers['user-agent'];
   try {
     reply.type('image/png').code(200);
-    return screenshot(request.query.url);
+    return screenshot(request.query.url, UA);
   } catch (e) {
     reply.code(500);
     return e;
   }
 });
 
-fastify.listen(process.env.PORT || 3000, '0.0.0.0', err => {
+fastify.listen(process.env.PORT || 3001, '0.0.0.0', err => {
   if (err) throw err;
   console.log(`server listening on ${fastify.server.address().port}`);
 });
--- src/screenshoter.js
+++ src/screenshoter.js
@@ -3,13 +3,18 @@
 const puppeteer = require('puppeteer');
 const devices = require('puppeteer/DeviceDescriptors');
 
-module.exports = async url => {
+module.exports = async (url, UA) => {
   try {
     const browser = await puppeteer.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     const page = await browser.newPage();
     // await page.emulate(devices['iPhone 6']);
+    // 1280x800 by default
+    await page.emulate({
+      userAgent: UA,
+      viewport: { width: 1280, height: 800 },
+    });
     await page.goto(url, {
       waitUntil: 'networkidle0',
     });
